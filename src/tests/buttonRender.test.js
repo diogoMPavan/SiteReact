715e255b60f2efeb3127aa9d86d56feca4fe672f
diogoMPavan <pavandiogo531@gmail.com>
2025-06-28 14:05:18 -0300

    Remove files

--- src/tests/buttonRender.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from '../pages/Login';
-
-test('Renderiza botão de entrar na tela de login', () => {
-  render(<Login />);
-  expect(screen.getByRole('button', { name: /Entrar/i })).toBeInTheDocument();
-});
--- src/tests/formatDate.test.js
+++
@@ -1,6 +0,0 @@
-import { formatDate } from "../utils/formatDate";
-
-test('Format date to yyyy-mm-dd', () => {
-    const result = formatDate('05/04/2025');
-    expect(result).toBe('2025-05-04');
-});
