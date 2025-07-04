6e09c33fe62a002f1e4916d46322aa1d802f70a6
diogoMPavan <pavandiogo531@gmail.com>
2025-07-04 18:59:22 -0300

    Ajuste na estrutura do projeto

--- sitereact/frontend/src/components/PrivateRoute.js
+++ sitereact/frontend/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../components/AuthContext';
+import { useAuth } from './AuthContext';
 import { AuthProvider } from './AuthContext';
 
 const PrivateRoute = ({ children }) => {
